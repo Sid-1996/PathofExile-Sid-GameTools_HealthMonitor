151243fb78f84a74c696bc546dabdf7c482cfd94
Sid-1996 <sid1996@example.com>
2026-08-30 18:39:33 +0800

    chore: release v1.3.2

--- src/_version.py
+++ src/_version.py
@@ -1,7 +1,6 @@
 # Single source of truth for version
 # Managed by commitizen (cz bump)
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 __author__ = "Sid"
 __github__ = "https://github.com/Sid-1996/PathofExile-Sid-GameTools_HealthMonitor"
-
 
